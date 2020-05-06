c544a6b2ca1eca72a9d2cf1b5d219fb411e4a29d
Mikhailmoor74 <mikhail.moor@bk.ru>
2020-05-06 20:39:31 +0500

    add first Toolbar

--- app/src/main/java/mikhailmoor/countryquiz/GameActivity.java
+++ app/src/main/java/mikhailmoor/countryquiz/GameActivity.java
@@ -1,17 +1,24 @@
 package mikhailmoor.countryquiz;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+
+import com.google.android.material.appbar.MaterialToolbar;
 
 public class GameActivity extends AppCompatActivity {
 
     private Button europaButton;
     private Button asiaButton;
     private Button oceaniaButton;
+
+    private MaterialToolbar toolbar;
 
 
     @Override
@@ -22,6 +29,14 @@ public class GameActivity extends AppCompatActivity {
         europaButton = findViewById(R.id.europaButton);
         asiaButton = findViewById(R.id.asiaButton);
         oceaniaButton = findViewById(R.id.oceaniaButton);
+        toolbar = findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null){
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_home_white_24dp);
     }
 
 
@@ -39,4 +54,20 @@ public class GameActivity extends AppCompatActivity {
         Intent intent = new Intent(this, OceaniaGame.class);
         startActivity(intent);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.toolbarmenu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
